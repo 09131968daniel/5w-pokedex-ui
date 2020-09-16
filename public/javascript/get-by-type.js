8fdc5f84cd4301d6718bb8f34d79e505f845faab
Philip Horton <philip.horton@att.net>
2020-09-16 13:27:03 -0600

    Added types options

--- public/javascript/get-by-type.js
+++ public/javascript/get-by-type.js
@@ -1,4 +1,20 @@
+const typeSelect = document.getElementById("typeSelect");
+
 async function searchByType(type) {
     let type_json = await fetch("https://pokeapi.co/api/v2/type/" + type).json();
     return type_json.pokemon;
 }
+
+function listTypes() {
+    const types = ["Normal", "Flying", "Fighting", "Fire", "Water",
+        "Electric", "Grass", "Bug", "Poison", "Dark", "Psychic", "Ghost",
+        "Ground", "Rock", "Steel", "Ice", "Dragon", "Fairy"];
+
+    for (let type of types) {
+        const option = document.createElement("option");
+        option.innerText = type;
+        typeSelect.append(option);
+    }
+}
+
+listTypes();
